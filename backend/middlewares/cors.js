ca15ee695a29e7647877c3f32ab4bd5884d9d95a
BarBoriska89 <mrs.ray.89@yandex.ru>
2023-08-04 16:24:49 +0300

    pr 15 19

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,16 +6,11 @@ const corsMW = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  console.log(`origin=${origin}`);
-  console.log(method);
-  console.log(requestHeaders);
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   if (method === 'OPTIONS') {
-    console.log('Я в предварительом запросе');
-    res.header('Access-Control-Aow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.end();
   }
