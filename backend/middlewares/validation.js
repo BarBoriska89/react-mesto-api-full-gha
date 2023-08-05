25ff3c9a19c3212027561b109ed5361a8ac9c7f6
BarBoriska89 <mrs.ray.89@yandex.ru>
2023-08-06 00:52:05 +0300

    pr 15 33

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -16,14 +16,14 @@ const createUserValidation = celebrate({
 
 const updateUserValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 });
 
 const updateAvatarValidation = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(regex),
+    avatar: Joi.string().pattern(regex).required(),
   }),
 });
 
